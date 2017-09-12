c336663afebb60a7b430015426a5958ee7eb79b5
Julie <julie.hoang.viet.nguyen@ocsbstudent.ca>
2017-09-12 19:12:53 -0400

    Initial Commit

---
+++ hello_world_GUI-1.py
@@ -0,0 +1,10 @@
+# Created by: Julie Nguyen
+# Created on: Sept 2017
+# Created for: ICS3U
+# Daily Assignment — Unit0-02
+# This program is the Hello, World program, but in GUI
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
